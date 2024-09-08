463dc6ac763a6cf9b519b18a37c1f18340ee3f01
Flavio <flavio@hevelop.com>
2024-09-08 15:53:35 +0200

    all stats showed

---
+++ src/exampleScore.js
@@ -0,0 +1,29 @@
+const exampleScore = `\`\`\`md
+------------------ VICTORY ------------------
+---------------------------------------------
+< USER >                         Omnia1989
+< DATE TIME >                    09/02/24 23:18
+< GAME MODE >                    CLASSIC
+< DIFFICULTY >                   INFERNO 10
+< CLASS >                        NECROMANCER
+< CLASS BONUS >                  PAIN ABSORPION
+< TOTAL SCORE >                  3852
+< TIME >                         0:46:35
++++++++++++++++++++++++++++++++++++++++++++++
+< CLASS UPGRADE >                YES
+< SKILLS > Swamp Sigil - Toxic Globe - Knowledge of Resistance - Shield of Shadows - Golden Shield - Toxic Strike - Knowledge of Strength - Heavy Shield
+< DECK > Skeleton Fighter - Skeleton Fighter - Skeleton Fighter - Skeleton Fighter - Banshee - Banshee - Mace Skeleton - Mace Skeleton - Medusa II - Beast Commander [BEASTMAN] - Seeker of the Light - Wizard II - Beast Commander [BEASTMAN]
++++++++++++++++++++++++++++++++++++++++++++++
+< FLOORS CLIMBED >               50
+< NORMAL ENEMIES KILLED >        15
+< ELITE KILLED >                 9
+< BOSS KILLED >                  6
+< GOLD EARNED >                  1305
+< GEMS EARNED >                  63
+< CARDS PLAYED >                 519
+< HIGHEST DAMAGE >               87
+< DAMAGE TAKEN >                 193
+< CHEST OPENED >                 6
+< GAME VERSION >                 3.5.1.6
+< VERIFICATION CODE >            0a343
+\`\`\``;
--- src/index.js
+++ src/index.js
@@ -10,6 +10,9 @@ parseButton.addEventListener('click', () => {
     formatResult(result);
 });
 
+// TODO: remove
+scoreInput.value = exampleScore;
+
 const parseScore = (score) => {
     if (score.startsWith('```md\n')) {
         score = score.replace('```md\n', '');
@@ -41,9 +44,16 @@ const parseRow = (row) => {
         };
     }
 
+    const key = groupes[1].trim();
+    var value = groupes[2].trim();
+
+    if (key !== "USER") {
+        value = value.toLowerCase();
+    }
+
     return {
-        key: groupes[1].trim(),
-        value: groupes[2].trim().toLowerCase()
+        key,
+        value
     };
 };
 
@@ -74,14 +84,14 @@ const getHeroSubclassImageName = (heroClass, subclass) => {
     return `badge_skill_${heroClass}_${subIndex + 1}`;
 }
 
-function checkImageExists(url, callback) {
+const checkImageExists = (url, callback) => {
     const img = new Image();
     img.onload = () => callback(true);
     img.onerror = () => callback(false);
     img.src = url;
 }
 
-function getImageSrc(element, imgName) {
+const getImageSrc = (element, imgName) => {
     const pngPath = `assets/${imgName}.png`;
     const jpgPath = `assets/${imgName}.jpg`;
 
@@ -100,6 +110,33 @@ function getImageSrc(element, imgName) {
     });
 }
 
+const titleCase = (str) => {
+    return str.toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+};
+
+const getGroupedArray = (array) => {
+    let index = 0;
+    const arrayCounter = array.reduce((acc, element) => {
+        if (!acc[element]) {
+            acc[element] = {
+                index,
+                count: 1
+            };
+        } else {
+            acc[element].count++;
+        }
+        index++;
+        return acc;
+    }, {});
+    return res = Object.entries(arrayCounter).map(([element, { index, count }]) => {
+        return {
+            name: element,
+            index,
+            count
+        };
+    }).sort((a, b) => a.index - b.index);
+};
+
 const formatResult = (result) => {
 
     // get class 
@@ -109,12 +146,10 @@ const formatResult = (result) => {
     heroClassImg.classList.add('shield');
     outputField.insertAdjacentElement('beforeend', heroClassImg);
 
-    // put hr
-    // outputField.insertAdjacentHTML('beforeend', '<hr>');
-
     // class bonuses container
     const classBonuses = document.createElement('div');
     classBonuses.classList.add('bonus');
+    outputField.insertAdjacentElement('beforeend', classBonuses);
 
     // get class base skill
     const classIcon = heroSkills[heroClass];
@@ -137,49 +172,203 @@ const formatResult = (result) => {
     if (classBonus) {
         const classBonusImg = document.createElement('img');
         getImageSrc(classBonusImg, getHeroSubclassImageName(heroClass, classBonus));
-        console.info(classBonusImg);
         classBonuses.insertAdjacentElement('beforeend', classBonusImg);
     }
-
-    outputField.insertAdjacentElement('beforeend', classBonuses);
-
-    // put hr
-    // outputField.insertAdjacentHTML('beforeend', '<hr>');
     
     // skills container
     const skillsContainer = document.createElement('div');
     skillsContainer.classList.add('skills');
+    outputField.insertAdjacentElement('beforeend', skillsContainer);
 
     // skills
     const skills = result["SKILLS"];
     const skillsArray = skills.split('-').map(skill => skill.trim());
-    skillsArray.forEach(skill => {
-        const imgName = heroSkills[skill];
-        const skillImg = document.createElement('img');
-        getImageSrc(skillImg, imgName);
-        skillsContainer.insertAdjacentElement('beforeend', skillImg);
-    });
-    outputField.insertAdjacentElement('beforeend', skillsContainer);
+    getGroupedArray(skillsArray).forEach(skill => {
+        const skillContainer = document.createElement('div');
+        skillContainer.classList.add('skill');
+        skillsContainer.insertAdjacentElement('beforeend', skillContainer);
 
-    // put hr
-    // outputField.insertAdjacentHTML('beforeend', '<hr>');
+        const skillImg = document.createElement('img');
+        skillContainer.insertAdjacentElement('beforeend', skillImg);
+        const imgName = heroSkills[skill.name];
+        getImageSrc(skillImg, imgName);
+
+        if (skill.count > 1) {
+            const skillText = document.createElement('span');
+            skillText.classList.add('counter');
+            skillText.textContent = `x ${skill.count}`;
+            skillContainer.insertAdjacentElement('beforeend', skillText);
+        }
+
+    });
 
     // cards container
     const cardsContainer = document.createElement('div');
     cardsContainer.classList.add('cards');
+    outputField.insertAdjacentElement('beforeend', cardsContainer);
 
     // deck
     const deck = result["DECK"];
     const deckArray = deck.split('-').map(card => card.trim());
-    deckArray.forEach(card => {
-        const imgName = heroCards[card];
+    getGroupedArray(deckArray).forEach(card => {
+        const imgContainer = document.createElement('div');
+        imgContainer.classList.add('card');
+        cardsContainer.insertAdjacentElement('beforeend', imgContainer);
+
         const cardImg = document.createElement('img');
+        imgContainer.insertAdjacentElement('beforeend', cardImg);
+        const imgName = heroCards[card.name];
         getImageSrc(cardImg, imgName);
-        cardsContainer.insertAdjacentElement('beforeend', cardImg);
+
+        if (card.count > 1) {
+            const cardText = document.createElement('span');
+            cardText.classList.add('counter');
+            cardText.textContent = `x ${card.count}`;
+            imgContainer.insertAdjacentElement('beforeend', cardText);
+        }
     });
-    outputField.insertAdjacentElement('beforeend', cardsContainer);
+    
+    // score container
+    const scoreContainer = document.createElement('div');
+    scoreContainer.classList.add('score');
+    outputField.insertAdjacentElement('beforeend', scoreContainer);
+
+    // user - date and time
+    const user = result["USER"];
+    const time = result["TIME"];
+    const dateTime = result["DATE TIME"];
+    const dateTimeUserText = document.createElement('span');
+    dateTimeUserText.textContent = ` at ${dateTime} in ${time}`;
+    const userText = document.createElement('span');
+    userText.textContent = user;
+    userText.classList.add('user');
+    dateTimeUserText.insertAdjacentElement('afterbegin', userText);
+    scoreContainer.insertAdjacentElement('beforeend', dateTimeUserText);
+
+    // mode and score
+    const mode = result["GAME MODE"];
+    const difficulty = result["DIFFICULTY"];
+    const score = result["TOTAL SCORE"];
+    const modeText = document.createElement('div');
+    modeText.textContent = `${titleCase(mode)} - ${titleCase(difficulty)} - Score: ${score}`;
+    scoreContainer.insertAdjacentElement('beforeend', modeText);
+
+    // enemies
+    const enemiesContainer = document.createElement('div');
+    enemiesContainer.classList.add('horizontal-stats');
+    scoreContainer.insertAdjacentElement('beforeend', enemiesContainer);
+
+    // normal
+    const normalEnemies = document.createElement('div');
+    normalEnemies.classList.add('single-stat');
+    const normalEnemiesImg = document.createElement('img');
+    getImageSrc(normalEnemiesImg, 'map_normal');
+    normalEnemies.textContent = result["NORMAL ENEMIES KILLED"];
+    normalEnemies.insertAdjacentElement('afterbegin', normalEnemiesImg);
+    enemiesContainer.insertAdjacentElement('beforeend', normalEnemies);
+    
+    // elite
+    const eliteEnemies = document.createElement('div');
+    eliteEnemies.classList.add('single-stat');
+    const eliteEnemiesImg = document.createElement('img');
+    getImageSrc(eliteEnemiesImg, 'map_elite');
+    eliteEnemies.textContent = result["ELITE KILLED"];
+    eliteEnemies.insertAdjacentElement('afterbegin', eliteEnemiesImg);
+    enemiesContainer.insertAdjacentElement('beforeend', eliteEnemies);
+
+    // boss
+    const bossEnemies = document.createElement('div');
+    bossEnemies.classList.add('single-stat');
+    const bossEnemiesImg = document.createElement('img');
+    getImageSrc(bossEnemiesImg, 'map_boss');
+    bossEnemies.textContent = result["BOSS KILLED"];
+    bossEnemies.insertAdjacentElement('afterbegin', bossEnemiesImg);
+    enemiesContainer.insertAdjacentElement('beforeend', bossEnemies);
+
+    // floors - chest opened - gold - gems
+    const gainContainer = document.createElement('div');
+    gainContainer.classList.add('horizontal-stats');
+    scoreContainer.insertAdjacentElement('beforeend', gainContainer);
+
+    // floors
+    const floors = document.createElement('div');
+    floors.classList.add('single-stat');
+    const floorsImg = document.createElement('img');
+    getImageSrc(floorsImg, 'map_tower_end');
+    floors.textContent = result["FLOORS CLIMBED"];
+    floors.insertAdjacentElement('afterbegin', floorsImg);
+    gainContainer.insertAdjacentElement('beforeend', floors);
+
+    // chest opened
+    const chest = document.createElement('div');
+    chest.classList.add('single-stat');
+    const chestImg = document.createElement('img');
+    getImageSrc(chestImg, 'map_treasure');
+    chest.textContent = result["CHEST OPENED"];
+    chest.insertAdjacentElement('afterbegin', chestImg);
+    gainContainer.insertAdjacentElement('beforeend', chest);
+
+    // gold
+    const gold = document.createElement('div');
+    gold.classList.add('single-stat');
+    const goldImg = document.createElement('img');
+    getImageSrc(goldImg, 'coin_icon');
+    gold.textContent = result["GOLD EARNED"];
+    gold.insertAdjacentElement('afterbegin', goldImg);
+    gainContainer.insertAdjacentElement('beforeend', gold);
+
+    // gems
+    const gems = document.createElement('div');
+    gems.classList.add('single-stat');
+    const gemsImg = document.createElement('img');
+    getImageSrc(gemsImg, 'gem_icon');
+    gems.textContent = result["GEMS EARNED"];
+    gems.insertAdjacentElement('afterbegin', gemsImg);
+    gainContainer.insertAdjacentElement('beforeend', gems);
+
+    // cards played - highest damage - damage taken
+    const doneContainer = document.createElement('div');
+    doneContainer.classList.add('horizontal-stats');
+    scoreContainer.insertAdjacentElement('beforeend', doneContainer);
+
+    // cards played
+    const cardsPlayed = document.createElement('div');
+    cardsPlayed.classList.add('single-stat');
+    const cardsPlayedImg = document.createElement('img');
+    getImageSrc(cardsPlayedImg, 'tab_card_icon_small');
+    cardsPlayed.textContent = result["CARDS PLAYED"];
+    cardsPlayed.insertAdjacentElement('afterbegin', cardsPlayedImg);
+    doneContainer.insertAdjacentElement('beforeend', cardsPlayed);
+    
+    // highest damage
+    const highestDamage = document.createElement('div');
+    highestDamage.classList.add('single-stat');
+    const highestDamageImg = document.createElement('img');
+    getImageSrc(highestDamageImg, 'old_skill_paladin');
+    highestDamage.textContent = result["HIGHEST DAMAGE"];
+    highestDamage.insertAdjacentElement('afterbegin', highestDamageImg);
+    doneContainer.insertAdjacentElement('beforeend', highestDamage);
+
+    // damage taken
+    const damageTaken = document.createElement('div');
+    damageTaken.classList.add('single-stat');
+    const damageTakenImg = document.createElement('img');
+    getImageSrc(damageTakenImg, 'slash');
+    damageTaken.textContent = result["DAMAGE TAKEN"];
+    damageTaken.insertAdjacentElement('afterbegin', damageTakenImg);
+    doneContainer.insertAdjacentElement('beforeend', damageTaken);
+
+    // game version, small upper right of scoreContainer
+    const gameVersion = result["GAME VERSION"];
+    const gameVersionText = document.createElement('span');
+    gameVersionText.textContent = `v${gameVersion}`;
+    gameVersionText.classList.add('game-version');
+    scoreContainer.insertAdjacentElement('beforeend', gameVersionText);
 
     // NTH: recuperare gli sfondi presenti
+
+    //< GAME VERSION > in alto a dx in piccolo
+    //< VERIFICATION CODE > non credo di mostrarlo
 
     // TODO: game mode (testo)
     // TODO: difficulty (testo)
